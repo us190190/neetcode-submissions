2442d002a38f71b323c506a8f603986bb01a0111
Utkarsh Singh <us190190@gmail.com>
2026-08-26 13:13:05 +0530

    Add: islands-and-treasure - submission-5

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        ROWS, COLS = len(grid), len(grid[0])
+        DIRECTIONS = [[1,0], [0,1], [-1,0], [0,-1]]
+        LAND, WATER, TREASURE = 2147483647, -1, 0
+
+        level = deque()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == TREASURE:
+                    level.append((r,c))
+        
+
+        distance = 0
+
+        while level:
+            length = len(level)
+            for _ in range(length):
+                r, c = level.popleft()
+                for dr, dc in DIRECTIONS:
+                    dr += r
+                    dc += c
+                    if 0<=dr<ROWS and 0<=dc<COLS and grid[dr][dc] == LAND:
+                        grid[dr][dc] = distance + 1
+                        level.append((dr, dc))
+            distance += 1
+            
+
+        
+
+
+        
