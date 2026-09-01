7688f71cf8edb4e84c88e91745bcf9f1082795ca
Utkarsh Singh <us190190@gmail.com>
2026-09-01 17:12:59 +0530

    Add: reverse-bits - submission-6

---
+++ Data Structures & Algorithms/reverse-bits/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+
+        count = 32
+        rev = 0
+
+        while count:
+            rev <<= 1
+            rev |= n&1
+            n >>= 1
+            count -= 1
+        
+        return rev
+
+
+        
